bdd52474990707f090c144bdfbdb76e003175d70
Fredrik Norén <fredrik.jw.noren@gmail.com>
2023-10-10 14:38:31 +0200

    Start on movement

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,14 @@
-use ambient_api::prelude::*;
+use ambient_api::{camera::screen_position_to_world_ray, core::messages::Frame, prelude::*};
+use packages::this::messages::Click;
 
 #[main]
-pub fn main() {}
+pub fn main() {
+    Frame::subscribe(|_| {
+        let (delta, cur) = input::get_delta();
+        if delta.mouse_buttons.contains(&MouseButton::Left) {
+            let ray =
+                screen_position_to_world_ray(camera::get_active().unwrap(), cur.mouse_position);
+            Click::new(ray.origin, ray.dir).send_server_reliable();
+        }
+    });
+}
--- src/server.rs
+++ src/server.rs
@@ -6,20 +6,26 @@ use ambient_api::{
         camera::concepts::{
             PerspectiveInfiniteReverseCamera, PerspectiveInfiniteReverseCameraOptional,
         },
+        physics::components::plane_collider,
         player::components::is_player,
         prefab::components::prefab_from_url,
         primitives::components::quad,
         transform::{
-            components::{lookat_target, translation},
-            concepts::Transformable,
+            components::{local_to_world, lookat_target, rotation, translation},
+            concepts::{Transformable, TransformableOptional},
         },
     },
-    entity::add_components,
+    entity::{add_component, add_components, set_component},
     prelude::*,
 };
 use packages::{
-    dead_meets_lead_content::assets, game_object::components::health,
-    unit_schema::components::run_direction, zombie_anims::components::zombie_anims,
+    dead_meets_lead_content::assets,
+    game_object::components::health,
+    this::{components::run_to, messages::Click},
+    unit_schema::components::{
+        is_on_ground, jumping, run_direction, running, speed, vertical_velocity,
+    },
+    zombie_anims::components::zombie_anims,
 };
 
 #[main]
@@ -37,12 +43,22 @@ pub fn main() {
     .with(lookat_target(), vec3(0., 0., 0.))
     .spawn();
 
+    Entity::new()
+        .with(local_to_world(), Mat4::IDENTITY)
+        .with(plane_collider(), ())
+        .with(quad(), ())
+        .spawn();
+
     spawn_query(is_player()).bind(|players| {
         for (id, _) in players {
             let zombie = Entity::new()
                 .with_merge(Transformable {
                     local_to_world: Default::default(),
-                    optional: Default::default(),
+                    optional: TransformableOptional {
+                        translation: Some(Vec3::Z * 10.),
+                        rotation: Some(Quat::IDENTITY),
+                        scale: None,
+                    },
                 })
                 .with(
                     prefab_from_url(),
@@ -50,7 +66,12 @@ pub fn main() {
                 )
                 .with(zombie_anims(), EntityId::null())
                 .with(run_direction(), Vec2::ZERO)
-                .with(health(), 100.);
+                .with(health(), 100.)
+                // .with_merge(CharacterMovment {})
+                .with(vertical_velocity(), 0.)
+                .with(running(), false)
+                .with(jumping(), false)
+                .with(is_on_ground(), false);
             add_components(id, zombie);
 
             // let idle = PlayClipFromUrlNodeRef::new(assets::url(
@@ -58,6 +79,23 @@ pub fn main() {
             // ));
             // let anim_player = AnimationPlayerRef::new(idle);
             // entity::add_component(id, apply_animation_player(), anim_player.0);
+        }
+    });
+
+    Click::subscribe(|cx, ev| {
+        if let Some(hit) = physics::raycast_first(ev.orig, ev.dir) {
+            println!("hit: {:?}", hit);
+            let id = cx.client_entity_id().unwrap();
+            add_component(id, run_to(), hit.position);
+        }
+    });
+
+    query((run_to(), translation())).each_frame(|entities| {
+        for (id, (target, pos)) in entities {
+            let dir = (target - pos).normalize();
+            let rot = dir.y.atan2(dir.x);
+            set_component(id, run_direction(), Vec2::X);
+            set_component(id, rotation(), Quat::from_rotation_z(rot));
         }
     });
 }
