3dca1573202e6178886fbeb55c34049ef1d77de8
Fredrik Norén <fredrik.jw.noren@gmail.com>
2023-10-20 17:36:02 +0200

    Fixes

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,21 @@
 use ambient_api::{camera::screen_position_to_world_ray, core::messages::Frame, prelude::*};
-use packages::this::messages::Click;
+use packages::{
+    orbit_camera::concepts::{OrbitCamera, OrbitCameraOptional},
+    this::messages::Click,
+};
 
 #[main]
 pub fn main() {
+    // OrbitCamera {
+    //     is_orbit_camera: (),
+    //     optional: OrbitCameraOptional {
+    //         lookat_target: Some(vec3(0., 0., 1.)),
+    //         camera_angle: Some(vec2(135f32.to_radians(), 45f32.to_radians())),
+    //         camera_distance: Some(3.0),
+    //     },
+    // }
+    // .spawn();
+
     Frame::subscribe(|_| {
         let (delta, cur) = input::get_delta();
         if delta.mouse_buttons.contains(&MouseButton::Left) {
